76dfdb2f6b75a9d62aaeb6f1f808be4f93a38356
Mortaneous <mortaneous.git@gmail.com>
2018-08-22 00:08:37 +0800

    Added TestHarness.java and cleaned up the code

--- src/com/mortaneous/misc/TaskScheduler/TaskDuration.java
+++ src/com/mortaneous/misc/TaskScheduler/TaskDuration.java
@@ -8,9 +8,6 @@ import java.time.Duration;
  
 public class TaskDuration
 {
-//	private int days;
-//	private int hours;
-//	private int minutes;
 	private Duration duration;
 	private String textDuration;
 	
@@ -73,75 +70,3 @@ public class TaskDuration
       return duration.toString();
     }
 }
-
-/*
-import java.time.Duration;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
- 
-public class TestHarness
-{
-  public static void main(String[] args)
-  {
-    int minutes;
-    Calendar cal = new GregorianCalendar(2018, 9, 31, 13, 35);
-    System.out.println(">> " + getDisplayString(cal));
-    minutes = 30;
-    cal.add(Calendar.MINUTE, 1*24*60);
-    System.out.println("plus 1d     >> " + getDisplayString(cal));
-    cal.add(Calendar.MINUTE, minutes);
-    System.out.println("plus 30mins  >> " + getDisplayString(cal));
-    minutes = 1440; // 1 day
-    cal.add(Calendar.MINUTE, minutes);
-    System.out.println("plus 24hours >> " + getDisplayString(cal));
-    cal.add(Calendar.MINUTE, 43200);
-    System.out.println("plus 30d     >> " + getDisplayString(cal));
-    
-    Calendar cal2 = (GregorianCalendar) cal.clone();
-    System.out.println("cal2 >> " + getDisplayString(cal2));
-    cal.add(Calendar.MINUTE, 43200);
-    System.out.println("plus 30d (cal2) >> " + getDisplayString(cal2));
-    System.out.println("cal1 >> " + getDisplayString(cal));
-    
-    
-    TaskDuration[] t = { new TaskDuration(1, 22, 3),
-          				 new TaskDuration(0, 0, 3),
-    					 new TaskDuration(0, 2, 0),
-    					 new TaskDuration(1, 0, 0),
-    					 new TaskDuration(1, 0, 3),
-    					 new TaskDuration(1, 2, 0),
-                         new TaskDuration(0, 2, 3), 
-                         new TaskDuration(-1, 2, 3), 
-                         new TaskDuration(0, -2, 0), 
-                         new TaskDuration(0, 2, -3), 
-                       };
-    
-    System.out.println("*** CONSTRUCTOR ***");
-    for(int i=0; i<t.length; i++) {
-      System.out.println("(" + i + ") " + t[i].getTextDuration());
-    }
-    
-    System.out.println("\n*** PROPERTY SETTER ***");
-    t[1].setDuration(0, 2, -3);
-    t[2].setDuration(1, 23, 3);
-    t[3].setDuration(0, 2, 0);
-    for(int i=1; i<4; i++) {
-      System.out.println("(" + i + ") " + t[i].getTextDuration() + " (" + t[i] + ")");
-    }
-    
-    System.out.println("\n***\nt[0] = " + t[0].getTextDuration());
-    System.out.println("total days    = " + t[0].getTotalDays());
-    System.out.println("total hours   = " + t[0].getTotalHours());
-    System.out.println("total minutes = " + t[0].getTotalMinutes());
-    
-  }
-  
-  public static String getDisplayString(Calendar cal)
-  {
-    return (cal.get(Calendar.MONTH)+1) + "/" + cal.get(Calendar.DAY_OF_MONTH) + "/" + cal.get(Calendar.YEAR) + " " +
-    	   cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE);
-    
-  }
-}
-
-*/
